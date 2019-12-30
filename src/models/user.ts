cf2bbb71dfb1ef20b254bf0bff4723df1dc66efb
gao-shan <sam.mosca@outlook.com>
2019-12-30 16:54:20 -0500

    register user component

---
+++ src/models/user.ts
@@ -0,0 +1,18 @@
+export class User {
+    userId: number;
+    username: String; 
+    firstName: String;
+    lastName: String;
+    email: String;
+    created: Date;
+    password: String;
+    constructor(userId:number, username: String, firstName: String, lastName: String, email: String, created: Date, password: String){
+        this.userId = userId
+        this.username = username
+        this.firstName = firstName
+        this.lastName = lastName
+        this.email = email
+        this.created = created
+        this.password = password
+    }
+}
