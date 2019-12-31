270c3b55bf0d2355242b6154d7b5641ebb8f1b60
Zia pmr <gernersnc@gmail.com>
2019-12-30 20:49:37 -0500

    better navbar

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,61 +1,47 @@
 import React, { useState } from 'react';
-import { Collapse, Navbar, NavbarToggler, Nav, NavItem, NavbarBrand, DropdownItem, DropdownMenu, DropdownToggle, UncontrolledDropdown } from 'reactstrap';
+import { Collapse, Navbar, NavbarToggler, Nav, NavItem, NavbarBrand } from 'reactstrap';
 import { Link } from 'react-router-dom';
+
 
 // const NavBar = (props:any) => {
 // const [collapsed, setCollapsed] = useState(true);
 
 // const toggleNavbar = () => setCollapsed(!collapsed);
-  const NavBar = (props: any) => {
-    const [isOpen, setIsOpen] = useState(false);
-  
-    const toggle = () => setIsOpen(!isOpen);
-    
-    return (
-      <div id= "headmenu">
-        <Navbar style={{backgroundColor: '#6699cc'}} light expand="md">
-        {/* <Nav className="navbar navbar-light" style={{backgroundColor: 'blue'}}>
-          <input style={{backgroundColor: 'blue'}} type="text" /> */}
-          
-          <NavbarBrand href="/"><h3>Top Chat</h3></NavbarBrand>
-          <NavbarToggler onClick={toggle} />
-          <Collapse isOpen={isOpen} navbar>
-            <Nav className="mr-auto" navbar>
-           
-              <NavItem className="nav">
-              <UncontrolledDropdown nav inNavbar>
-              <DropdownToggle nav caret right>
-                User
-              </DropdownToggle>
-              <DropdownMenu right>
-                <DropdownItem>
-                    {/* Please, change the path */}
-                <Link to='/'>Home</Link>
-                </DropdownItem>
-                <DropdownItem>
-                <Link to='/'>User</Link>
-                </DropdownItem>
-                <DropdownItem>
-                <Link to='/'>Make Topic</Link> 
-                <Link to='/'>Logout</Link> 
-                
-                </DropdownItem>
-               
-                
-              </DropdownMenu>
-            </UncontrolledDropdown>
-            
-            </NavItem>
-            </Nav>
-          </Collapse>
-          <NavItem className="nav">
-            <Link to='/login' style={{color: 'black'}}>LOGIN</Link>
-          </NavItem>
-        </Navbar>
+const NavBar = (props: any) => {
+  const [isOpen, setIsOpen] = useState(false);
 
-      </div>
-    );
-        }
+  const toggle = () => setIsOpen(!isOpen);
+    
+    
+      return (
+        <div>
+          <Navbar color="light" light expand="md">
+            <NavbarBrand href="/"><h3>TopChat</h3></NavbarBrand>
+            <NavbarToggler onClick={toggle} />
+            <Collapse isOpen={isOpen} navbar>
+              <Nav className="mr-auto" navbar>
+              <NavItem className="nav">
+                <NavItem>
+                  <Link to='/'>Home</Link>
+                </NavItem>
+                <NavItem>
+                  <Link to='/'>User</Link>
+                </NavItem>
+                <NavItem>
+                  <Link to='/'>Make a Topic</Link>
+                </NavItem>
+                <NavItem>
+                  <Link to='/'>Logout</Link>
+                </NavItem>
+                </NavItem>
+              </Nav>
+              
+              
+            </Collapse>
+          </Navbar>
+        </div>
+      );
+  }
 export default NavBar;
 
 
